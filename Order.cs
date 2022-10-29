a6573e6b52cd968681bede798f7a16083e437f4a
archeryfox <archeryfox2005@gmail.com>
2022-10-29 13:32:24 +0300

    Auto-bash-commit

---
+++ Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Tortics
+{
+    public class Order
+    {
+        //дефолт конструктор, пока не трогать
+        public string Form = "";
+        public int Size = 0;
+        public string Taste = "";
+        public int Amount = 1;
+        public string Glaze = "";
+        public Decor Decor;
+
+        static public void MenuL0()
+        {
+            var n = "\n  ";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write($" Велком ту Кирби Кафетерий!!" +
+                $"\n Выберите компоненты торта:\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write("=");
+            }
+            Console.SetCursorPosition(2, 4 - 1);
+            Console.Write($"Форма" + n +
+                $"Размер" + n +
+                $"Вкус" + n +
+                $"Количество коржиков" + n +
+                $"Глазурь" + n +
+                $"Декор" + n + 
+                $"Я закончил");
+        }
+
+        public Order(int i)
+        {
+            Amount = i;
+        }
+        public Order(Decor decor, int amount, string view, string color, int size)
+        {
+            Decor = new Decor(amount, view, color, size);
+        }
+    }
+}
+
--- Runtime.cs
+++ Runtime.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tortics;
 
 namespace Cakes
 {
     internal class Terminal
     {
+        /// <summary>
+        /// Более комфортный путь создания через конструктор
+        /// </summary>
+        /// <param name="amount">Количество деталек декора</param>
+        /// <param name="view">Вид деталей</param>
+        /// <param name="color">Цвет деталей</param>
+        /// <param name="size">Введите размер деталек декора</param>
+        /// <returns>Объект сразу с декором</returns>
 
+        static Order OrderDecor(int amount = 0, string view = "", string color = "", int size = 0)
+        {
+            return new Order(new Decor(), amount, view, color, size);
+        }
+
+        static void Main()
+        {
+            Order.MenuL0();
+        }
     }
 }

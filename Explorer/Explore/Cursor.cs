53eee1770f0746474798f053c160742c2790c5c3
archeryfox <archeryfox2005@gmail.com>
2022-11-28 14:31:53 +0300

    initial branch

---
+++ Explorer/Explore/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explore
+{
+    public class Cursor
+    {
+        public int y;
+        public int min;
+        public int max;
+        public Cursor(int y, int min, int max)
+        {
+            this.y = y;
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
